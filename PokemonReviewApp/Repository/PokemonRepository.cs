eb72956754035f9b673e2d6bbc7153509323d03c
Jedd Whiteman <jeddwhiteman@gmail.com>
2023-06-19 18:57:42 +0200

    Add project files.

---
+++ PokemonReviewApp/Repository/PokemonRepository.cs
@@ -0,0 +1,20 @@
+﻿using PokemonReviewApp.Data;
+using PokemonReviewApp.Interfaces;
+using PokemonReviewApp.Models;
+
+namespace PokemonReviewApp.Repository
+{
+    public class PokemonRepository : IPokemonRepository
+    {
+        private readonly ApplicationDBContext _db;
+        public PokemonRepository(ApplicationDBContext Db)
+        {
+            _db = Db;
+        }
+
+        public ICollection<Pokemon> GetPokemons()
+        {
+            return _db.Pokemons.OrderBy(p => p.Id).ToList();
+        }
+    }
+}
